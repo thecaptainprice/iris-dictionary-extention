392b804c1b65e06df2d3a9418828daeb52dfcaae
Captain Price <reza.irisdev@gmail.com>
2023-05-08 22:14:37 +0330

    close button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'iris-dictionary-extention';
+  closeExtension(): void {
+    chrome.runtime.sendMessage({ type: 'close_extension' });
+  }
 }
--- src/background.js
+++ src/background.js
@@ -3,25 +3,43 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {
-
           schemes: ['http', 'https']
         },
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
-
 });
 
-chrome.alarms.onAlarm.addListener((...args) => {
-  console.warn('Found chrome alarm! >>> ', args);
-
+chrome.alarms.onAlarm.addListener((alarm) => {
+  console.warn('Found chrome alarm! >>> ', alarm);
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === 'closeExtension') {
-    chrome.tabs.getCurrent(function (tab) {
-      chrome.tabs.remove(tab.id);
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      console.log("---------------")
+      chrome.tabs.remove(tabs[0].id);
     });
   }
 });
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.type === 'close_extension') {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      console.log("==================")
+      chrome.tabs.remove(tabs[0].id);
+    });
+  }
+});
+
+chrome.idle.onStateChanged.addListener(
+  (browserActivityState) => {
+    console.log('browserActivityState changed')
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0].url.match('https:\/\/.*.reddit.com\/.*')) {
+        chrome.tabs.sendMessage(tabs[0].id, { browserActivityState: browserActivityState });
+      }
+    });
+  }
+)
